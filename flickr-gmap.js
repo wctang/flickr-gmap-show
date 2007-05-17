02b08b3622139601aa9987f4756237dcbe5c6fa9
wctang <wctang@6a657613-d72d-0410-a72d-ff86cfd08b3b>
2007-05-17 07:19:53 +0000

    release

--- flickr-gmap.js
+++
@@ -1,8 +0,0 @@
-document.write('<link type="text/css" media="screen" rel="stylesheet" href="http://flickr-gmap-show.googlecode.com/svn/trunk/lightbox/css/lightbox.css"/>');
-
-document.write('<script type="text/javascript" src="http://flickr-gmap-show.googlecode.com/svn/trunk/gmap2.js"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show.googlecode.com/svn/trunk/flickr-gmap-show.js"></script>');
-
-document.write('<script type="text/javascript" src="http://flickr-gmap-show.googlecode.com/svn/trunk/lightbox/js/prototype.js"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show.googlecode.com/svn/trunk/lightbox/js/scriptaculous.js?load=effects"></script>');
-document.write('<script type="text/javascript" src="http://flickr-gmap-show.googlecode.com/svn/trunk/lightbox/js/lightbox.js"></script>');
