18686b07979c22fa3ee4d428c3cd0a6ba10c8c44
wctang <wctang@6a657613-d72d-0410-a72d-ff86cfd08b3b>
2007-05-17 07:14:09 +0000

    release

---
+++ tags/fgs/current/fgs.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name          Flickr Gmap Show
+// @namespace     http://code.google.com/p/flickr-gmap-show/
+// @description   Flickr Gmap Show user script for greasemonkey
+// @include       http://www*.flickr.com/photos/*
+// ==/UserScript==
+
+(function() {
+
+var FGSSCRIPT = 'http://www.csie.nctu.edu.tw/~wctang/fgs/fgs.c.js';
+    
+var loc = /^http:\/\/www.*\.flickr\.com\/photos\/([a-zA-Z0-9\-\_@]*)\/sets\/(\d*)(\/.*)?$/(window.location.href);
+if(loc&&loc[1]&&loc[2]) {
+    var s = document.createElement('script');
+    s.type='text/javascript';
+    s.src=FGSSCRIPT;
+    document.getElementsByTagName('head')[0].appendChild(s);
+} else {
+    loc = /^http:\/\/www.*\.flickr\.com\/photos\/([a-zA-Z0-9\-\_@]*)\/(\d*)(\/.*)?$/(window.location.href);
+    if(loc&&loc[1]&&loc[2]) {
+        var locdiv = document.getElementById('li_location');
+        var geospan = document.getElementById('div_pre_geo_block');
+        if( locdiv && geospan) {
+            var s = document.createElement('script');
+            s.type='text/javascript';
+            s.src=FGSSCRIPT;
+            document.getElementsByTagName('head')[0].appendChild(s);
+        }
+    }
+}
+
+
+})();
